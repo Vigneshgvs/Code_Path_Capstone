224ce89740ae10f5f735d990243964b0a3cf9266
Jose Folgar <f.jose@wustl.edu>
2024-04-10 22:23:52 -0500

    created initial swiftui project

---
+++ codepath_capstone/codepath_capstone/codepath_capstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  codepath_capstoneApp.swift
+//  codepath_capstone
+//
+//  Created by Jose Folgar on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct codepath_capstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
